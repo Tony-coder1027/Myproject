de96af22e054a287735779e39eab2b8ec71f343d
TY10271 <3248741568@qq.com>
2023-07-08 19:21:31 +0800

    a commit 1

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -2,5 +2,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello JAVA");
         System.out.println("Hello LJS");
+        System.out.println("a test");
     }
 }
