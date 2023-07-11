76b8a2f30360570b44bfe7413e5ec4acd04142a1
TY10271 <3248741568@qq.com>
2023-07-11 20:25:34 +0800

    第三章 commit 1

--- src/main/java/C3语言基础/P55练习5.java
+++ src/main/java/C3语言基础/P55练习5.java
@@ -2,8 +2,14 @@ package C3语言基础;
 
 public class P55练习5 {
     public static void main(String[] args) {
-        int a = 123;
-        int b = a%120;
+        int number = 123;
+        int number1;
+        int number2;
+        int number3;
+        number1 = number % 10;//number1 = 3
+        number2 = number / 10 % 10;//number2 = 2
+        number3 = number / 100;//number3 = 1
+        System.out.println(number + "颠倒后为：" + number1 + number2 + number3);
 
     }
 }
---
+++ src/main/java/C3语言基础/draft.java
@@ -0,0 +1,7 @@
+package C3语言基础;
+
+public class draft {
+    public static void main(String[] args) {
+
+    }
+}
