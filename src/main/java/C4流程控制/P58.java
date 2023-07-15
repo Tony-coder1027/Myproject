7d2afe81c7669811e25b8a71ceea64da78dbd0c3
TY10271 <3248741568@qq.com>
2023-07-15 15:46:15 +0800

    第四章 commit 1

---
+++ src/main/java/C4流程控制/P58.java
@@ -0,0 +1,11 @@
+package C4流程控制;
+
+public class P58 {
+    public static void main(String[] args) {
+        int phonenumber = 123456789;
+        if (phonenumber != 897877){
+            System.out.println("您拨打的电话不存在");
+            
+        }
+    }
+}
